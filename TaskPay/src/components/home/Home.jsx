e7d9cb4e3b78e018d5c1d3e15a961dc47e5f0d86
Priyabrata33 <priyabratasahu4@gmail.com>
2024-07-11 19:28:52 +0530

    adding the second view point in home page

--- TaskPay/src/components/home/Home.jsx
+++ TaskPay/src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box';  // Assuming you're using Material-UI Box
-import { Button } from '@mui/base/Button';
+import Button from '@mui/material/Button';
 import './Home.css'
 
 function Home() {
@@ -7,9 +7,11 @@ function Home() {
     <>
       <Box className = "homeStyle">
         <div className='homeContent'>
-              <h1 style={{margin:'64px 0 0 0',padding:'0px',width:'fit-content' }}>BUILDING THE</h1>
-              <h1 style={{margin:'10px',padding:'3px', backgroundColor:'#f19433',width:'fit-content',borderRadius:'5px',color:'white'}}>NEW</h1> 
-              <h1 style={{margin:'0px',padding:'0px',width:'fit-content' }}>OPERATING SYSTEM FOR THE FUTURE OF MONEY MANAGEMENT IN INDIA</h1>
+              <h1>
+              <span>BUILDING THE </span>
+              <span style={{ backgroundColor:'#f19433', borderRadius:'5px',color:'white'}}> NEW</span> 
+              <span> OPERATING SYSTEM FOR THE FUTURE OF MONEY MANAGEMENT IN INDIA</span>
+              </h1>
         </div>
         <div className="container">
           <div className="columnLeft">
@@ -17,28 +19,42 @@ function Home() {
                 WELCOME TO TASK PAY
                 <span className="headingLine"></span> {/* Apply the gradient line here */}
               </h3>
-            <p style={{color:'#666'}}>TaskPay Automates Escrow Transactions &amp; <br/> Transforms The User Experience Whilst Managing <br/> Monies Through To Settlement</p>
+            <p>TaskPay Automates Escrow Transactions &amp; Transforms The User Experience Whilst Managing Monies Through To Settlement</p>
             <div style={{marginTop:'40px'}}>
-            
-            <Button
-              style={{
+            <Button sx={{
                 backgroundColor: '#f19433',
                 color: 'white',
-                borderRadius: '20px',
-                padding: '15px',
-                fontSize: '1.2rem',
-              }}
-            >
-              Talk to Our Export &gt;&gt;
-            </Button>
+                borderRadius: '40px',
+                fontFamily: 'Montserrat, sans-serif',
+                '&:hover': { color: '#FFA500', backgroundColor: 'transparent' },
+                padding:'14px 24px 14px 24px',
+                fontSize: '1rem',
+              }}>Talk to Our Export   &gt;&gt;</Button>
             </div>
           </div>
-          <div className="column">
+          <div className="imageColumn">
             <h1>Images are here</h1>
             {/* Add your images or other content here */}
           </div>
         </div>
+        
       </Box>
+      <Box className="secondView">
+          <div className='vector'>
+              <div className='vectorImage'>
+                <h1>Vector images</h1>
+              </div>
+              <div className='vectorContents'>
+                <div className='heading'>
+                  <h2>BUILD TO <span style={{color: 'orange'}}>SECURE</span> &amp; <span style={{color:"orange"}}>PROTECT</span></h2>
+                  <span className="headingLine"></span>
+                </div>
+                <p >Helping <span>65 millions SMEs to secure transactions from fraud and timely payments </span></p>
+                <p>Our <span>technology</span> enables buyers and sellers to <span>transact anywhere, anyime</span> via <span>any device</span>, in a safe and secure way. <span>Smater, faster, safer</span> always</p>
+              </div>
+          </div>
+      </Box>
+      
     </>
   );
 }
